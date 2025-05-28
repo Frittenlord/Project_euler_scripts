608bc93e9180199b45db3ea8283200c14117de85
Frittenlord <44476547+Frittenlord@users.noreply.github.com>
2025-05-28 15:02:21 +0200

    Create special_pythagorean_triplet.py

---
+++ #9 Special Pythagorean Triplet/special_pythagorean_triplet.py
@@ -0,0 +1,28 @@
+
+
+def find_py_triplet(n):
+    """
+    Generating triples by using Euklids formulas:
+
+    a = (x**2)-(y**2)
+    b = 2*(x*y)
+    c = (x**2)+(y**2)
+    
+    a+b+c = 2x(x+y) = n
+
+    Then just trying every number combination up to n
+    """
+
+    triples = {}
+    for x in range(n):
+        for y in range(n):
+            if 2*x*(x+y) == n:
+                a = (x**2)-(y**2)
+                b = 2*(x*y)
+                c = (x**2)+(y**2)
+                if not a <= 0 or b <= 0 or c <= 0: # make sure we stay positive
+                    triples[f'{a}, {b}, {c}'] = a*b*c
+    return triples
+
+
+print(find_py_triplet(1000))
